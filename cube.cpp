c08d1696ff7f39dd885752755b7cc438546228bd
HStaaf <staaf.henrik@gmail.com>
2025-02-11 09:24:29 +0100

    Added some stuff for materials to the cube class. specifically I added the part where a texture is loaded. Check in the constructor at the bottom and you will see

--- cube.cpp
+++ cube.cpp
@@ -14,6 +14,8 @@ Cube::Cube(ID3D11Device* dxdevice, ID3D11DeviceContext* dxdevice_context) : Mode
 	// Populate the vertex array with 4 Vertices
 	Vertex v0, v1, v2, v3, v4, v5, v6, v7;
 
+	#pragma region These things are fine, dont worry about it
+
 	#pragma region Creating Front face
 	v0.Position = { -0.5, -0.5f, 0.0f };
 	v0.Normal = { 0, 0, 1 };
@@ -169,7 +171,7 @@ Cube::Cube(ID3D11Device* dxdevice, ID3D11DeviceContext* dxdevice_context) : Mode
 	indices.push_back(23);
 	#pragma endregion
 
-
+	#pragma region Loading the vertex and index buffers
 	// Vertex array descriptor
 	D3D11_BUFFER_DESC vertexbufferDesc{ 0 };
 	vertexbufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
@@ -197,8 +199,24 @@ Cube::Cube(ID3D11Device* dxdevice, ID3D11DeviceContext* dxdevice_context) : Mode
 	// Create index buffer on device using descriptor & data
 	dxdevice->CreateBuffer(&indexbufferDesc, &indexData, &m_index_buffer);
 	SETNAME(m_index_buffer, "IndexBuffer");
+	#pragma endregion
 
+	#pragma endregion
+	
 	m_number_of_indices = (unsigned int)indices.size();
+
+	if (m_material.DiffuseTextureFilename.size()) {
+
+		HRESULT hr;
+
+		hr = LoadTextureFromFile(
+			dxdevice,
+			m_material.DiffuseTextureFilename.c_str(),
+			&m_material.DiffuseTexture);
+		std::cout << "\t" << m_material.DiffuseTextureFilename
+			<< (SUCCEEDED(hr) ? " - OK" : "- FAILED") << std::endl;
+	}
+
 }
 
 void Cube::Render() const
